7f99c2f13bf0c2782b32061970b973e775f8ff44
thaa <atmufi2006@gmail.com>
2026-04-29 01:28:06 +0700

    index & navbar

---
+++ halamanAwal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Gembira Loka</title>
+</head>
+<body id="halaman-awal">
+    <section class="hero d-flex align-items-center justify-content-center text-center">
+        
+        <div class="overlay"></div>
+
+        <div class="box">
+            <h1 class="fw-bold">Welcome to Gembira Loka</h1>
+            <p>
+                Pesan tiket online tanpa perlu antri!
+            </p>
+
+            <a href="index.php" class="btn custom-btn mt-3 px-4" style="font-size: 20px;">
+                Pesan Tiketmu Sekarang
+            </a>
+
+        </div>
+
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Tugas Akhir PWD</title>
+</head>
+<body>
+    <section class="hero hero-atas">
+        <nav class="navbar navbar-expand bg-light py-3">
+            <div class="container">
+                <a href="#" class="navbar-brand">Gembira Loka Zoo</a>
+
+                <div id="navmenu">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">kategori</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">about us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="judul">
+            <h3>Selamat Datang di</h3>
+            <h1><b>GEMBIRA LOKA <br>ZOO</b></h1>
+        </div>
+    </section>
+
+    
+</body>
+</html>
