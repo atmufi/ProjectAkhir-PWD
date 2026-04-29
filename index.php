4cc5872a054dbee59635a6270140b92542f14836
thaa <atmufi2006@gmail.com>
2026-04-30 01:09:28 +0700

    nambah index

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
     <section class="hero hero-atas">
         <nav class="navbar navbar-expand bg-light py-3">
             <div class="container">
-                <a href="#" class="navbar-brand">Gembira Loka Zoo</a>
+                <a href="#" class="navbar-brand">
+                    <img src="assets/img/logonobg.png" alt="logo">    
+                    Gembira Loka Zoo
+                </a>
 
                 <div id="navmenu">
                     <ul class="navbar-nav">
@@ -35,6 +38,96 @@
         </div>
     </section>
 
-    
+    <section class="kotak">
+            <div class="kotak-preview1">
+                <h1 id="pembelian"><b>Pembelian</b></h1>
+                <p>Tiket Gembira Loka Zoo</p>
+                <a href="tiket.php" class="btn btn-outline-dark mb-3" style="font-size: 20px;">
+                    Beli Tiket
+                </a>
+            </div>
+
+            <div class="kotak-preview2">
+                <h1 id="peta"><strong>Peta</strong></h1>
+                <p>Gembira Loka Zoo</p>
+                <a href="index.php" class="btn btn-outline-dark" style="font-size: 20px;">
+                    Lihat Peta
+                </a>
+            </div>
+            
+            <div class="kotak-preview3">
+                <h1 id="kategori">kategori</h1>
+                <p>Tiket Gembira Loka Zoo</p>
+                <a href="index.php" class="btn btn-outline-dark" style="font-size: 20px;">
+                    Cek Kategori
+                </a>
+            </div>
+    </section>
+
+
+    <section id="wahana">
+        <div class="container">
+            <div id="judul">
+                <p><strong id="besar">WAHANA</strong> GEMBIRA LOKA ZOO</p>
+            </div>
+
+            <div class="row pt-5 text-center">
+                <div class="col">
+                    <div class="card" style="width: 15rem;">
+                        <img src="assets/img/interaksigajah1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Interaksi Gajah Sumatra</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style="width: 15rem;">
+                        <img src="assets/img/pettingzoo.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Petting Zoo</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style="width: 15rem;">
+                        <img src="assets/img/terapiikan.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Terapi Ikan</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col">
+                    <div class="card" style="width: 15rem;">
+                        <img src="assets/img/atv.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">ATV</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
+
+<footer class="footer">
+  <div class="container">
+    
+    <h3>Gembira Loka Zoo</h3>
+    <p>Wisata edukasi dan rekreasi keluarga di Yogyakarta 🐾</p>
+
+    <div class="footer-menu">
+      <a href="#">Home</a>
+      <a href="#">Kategori</a>
+      <a href="#">About Us</a>
+    </div>
+
+    <p class="copy">© 2026 Gembira Loka Zoo. All rights reserved.</p>
+
+  </div>
+</footer>
+
 </html>
